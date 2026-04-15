5455cd6b8000ea4ba78fe1e04308783cad3ea9b7
Nothingmeta <the_sjoerd_@hotmail.com>
2026-04-15 23:20:04 +0200

    Extra role info test

--- cypress/component/role-info.cy.js
+++ cypress/component/role-info.cy.js
@@ -1,9 +1,9 @@
 require("../../Pubquiz Platform V2/wwwroot/js/components/role-info.js");
 
 describe("role-info", () => {
-  const mountWithSelect = () => {
-    cy.document().then((doc) => {
-      doc.body.innerHTML = `
+    const mountWithSelect = () => {
+        cy.document().then((doc) => {
+            doc.body.innerHTML = `
         <select id="roleSelect">
           <option value="">-- kies rol --</option>
           <option value="speler">Speler</option>
@@ -12,37 +12,60 @@ describe("role-info", () => {
         </select>
         <role-info for="roleSelect"></role-info>
       `;
+        });
+    };
+
+    it("shows the default hint when no role is selected", () => {
+        mountWithSelect();
+
+        cy.get("role-info")
+            .shadow()
+            .find(".box")
+            .should("contain.text", "Selecteer een rol om de beschrijving te zien.");
     });
-  };
 
-  it("shows the default hint when no role is selected", () => {
-    mountWithSelect();
+    it("shows speler description on select change", () => {
+        mountWithSelect();
 
-    cy.get("role-info")
-      .shadow()
-      .find(".box")
-      .should("contain.text", "Selecteer een rol om de beschrijving te zien.");
-  });
+        cy.get("#roleSelect").select("speler");
 
-  it("shows speler description on select change", () => {
-    mountWithSelect();
+        cy.get("role-info")
+            .shadow()
+            .find(".box")
+            .should("contain.text", "Speler: Neem deel aan quizzen, beantwoord vragen en bekijk je scores.");
+    });
 
-    cy.get("#roleSelect").select("speler");
+    it("shows quizmaster description on select change", () => {
+        mountWithSelect();
 
-    cy.get("role-info")
-      .shadow()
-      .find(".box")
-      .should("contain.text", "Speler: Neem deel aan quizzen, beantwoord vragen en bekijk je scores.");
-  });
+        cy.get("#roleSelect").select("quizmaster");
 
-  it("shows quizmaster description on select change", () => {
-    mountWithSelect();
+        cy.get("role-info")
+            .shadow()
+            .find(".box")
+            .should("contain.text", "Quizmaster: Maak en beheer quizzen, start lobbies en beheer spelers.");
+    });
 
-    cy.get("#roleSelect").select("quizmaster");
+    it("visually switches between roles in one run", () => {
+        mountWithSelect();
 
-    cy.get("role-info")
-      .shadow()
-      .find(".box")
-      .should("contain.text", "Quizmaster: Maak en beheer quizzen, start lobbies en beheer spelers.");
-  });
+        cy.get("role-info").shadow().find(".box")
+            .should("contain.text", "Selecteer een rol om de beschrijving te zien.");
+
+        cy.get("#roleSelect").select("speler");
+        cy.get("role-info").shadow().find(".box")
+            .should("have.text", "Speler: Neem deel aan quizzen, beantwoord vragen en bekijk je scores.");
+
+        cy.wait(800);
+
+        cy.get("#roleSelect").select("quizmaster");
+        cy.get("role-info").shadow().find(".box")
+            .should("have.text", "Quizmaster: Maak en beheer quizzen, start lobbies en beheer spelers.");
+
+        cy.wait(800);
+
+        cy.get("#roleSelect").select("-- kies rol --");
+        cy.get("role-info").shadow().find(".box")
+            .should("contain.text", "Selecteer een rol om de beschrijving te zien.");
+    });
 });
